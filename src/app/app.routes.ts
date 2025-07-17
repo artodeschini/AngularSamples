0b52b20a0daa2e98eecabfe0d6ae5993dc57b39c
Artur Todeschini Crestani <artodeschini@gmail.com>
2025-07-16 21:57:51 -0300

    watch start and stop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ProfileCard } from './compponents/profile-card/profile-card';
 import { Counter } from './compponents/counter/counter';
 import { TodoList } from './compponents/todo-list/todo-list';
+import { StopWatch } from './compponents/stop-watch/stop-watch';
 
 export const routes: Routes = [
 
     { path: 'card-profile', component: ProfileCard },
     { path: 'count', component: Counter },
-    { path: 'to-do-list', component: TodoList }
+    { path: 'to-do-list', component: TodoList },
+    { path: 'stop-watch', component: StopWatch }
     
 ];
---
+++ src/app/compponents/stop-watch/stop-watch.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-stop-watch',
+  imports: [CommonModule],
+  templateUrl: './stop-watch.html',
+  styleUrl: './stop-watch.scss'
+})
+export class StopWatch {
+
+  elapsedTime: number = 0;
+  isRunning: boolean = false;
+  intervalRef: any;
+
+  startStop() {
+    this.isRunning ? this.stop() : this.start();
+    /* if (this.isRunning) {
+      this.stop();
+    } else {
+      this.start();
+    } */
+  }
+
+
+  private start() {
+    this.isRunning = true;
+    this.intervalRef = setInterval(() => {
+      this.elapsedTime += 0.1; // Increment by 1 second
+    }, 100)
+    console.log("Stopwatch started");
+  }
+
+  private stop() {
+    this.isRunning = false;
+    clearInterval(this.intervalRef);
+    console.log("Stopwatch stopped");
+  }
+
+  reset() {
+    this.isRunning = false;
+    clearInterval(this.intervalRef);
+    console.log("Stopwatch reset");
+  }
+}
