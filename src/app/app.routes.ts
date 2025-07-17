d7d24091079cb45193dc192f611e978a31f439ab
Artur Todeschini Crestani <artodeschini@gmail.com>
2025-07-16 21:06:29 -0300

    add todo list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProfileCard } from './compponents/profile-card/profile-card';
 import { Counter } from './compponents/counter/counter';
+import { TodoList } from './compponents/todo-list/todo-list';
 
 export const routes: Routes = [
 
     { path: 'card-profile', component: ProfileCard },
     { path: 'count', component: Counter },
+    { path: 'to-do-list', component: TodoList }
     
 ];
