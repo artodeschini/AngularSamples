10b9e3196999efb6faea98122e7a2a9f661fde77
Artur Todeschini Crestani <artodeschini@gmail.com>
2025-07-16 21:00:54 -0300

    sample of counter componet interactive

--- src/app/compponents/counter/counter.ts
+++ src/app/compponents/counter/counter.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.html',
   styleUrl: './counter.scss'
 })
 export class Counter {
+
+  counter: number = 7;
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+
+  setZero() {
+    this.counter = 0;
+  }
+  
+  getCounterStatus(): string {
+    if (this.counter > 0) {
+      return 'positive';
+    } else if (this.counter === 0) {
+      return 'neutral';
+    } else {
+      return 'negative';
+    }
+  }
 
 }
